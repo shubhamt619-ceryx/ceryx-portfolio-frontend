1ab44041a80f204101f01f18d85fda0028fbec3d
philomathaf <yashthakur1915@gmail.com>
2022-04-21 23:03:05 +0530

    sample management

--- src/app/modules/samples-management/add-sample/add-sample.component.ts
+++ src/app/modules/samples-management/add-sample/add-sample.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import KTWizard from '../../../../assets/js/components/wizard';
 import { KTUtil } from '../../../../assets/js/components/util';
 
@@ -11,6 +11,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./add-sample.component.scss'],
   providers: [MessageService],
 })
+
 export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('wizard', { static: true }) el: ElementRef;
   submitted = false;
@@ -19,14 +20,27 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
   files1: TreeNode[] = [];
   selectedFile: TreeNode;
   nodeService: any;
+  uploadedFiles: any[] = []
+  uploadedImageFiles: any[] = []
+  currentCategory;
   subscriptions: Subscription[] = []
+  sampleId = "";
+  launchUrl = "";
+  thumbnail = "";
+  name = "";
+  details = "";
+  category = "";
+  tags:any[] = [];
 
   constructor(
     private messageService: MessageService,
     private commonService: CommonService,
+    private cd: ChangeDetectorRef,
     ) { }
 
   ngOnInit(): void {
+    this.messageService.clear()
+    this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'User updated', life: 3000 });
     let filesData = [
       {
           "label": "Folder 1",
@@ -89,19 +103,38 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
     this.wizard.on('beforeNext', (wizardObj) => {
       // https://angular.io/guide/forms
       // https://angular.io/guide/form-validation
-
       // validate the form and use below function to stop the wizard's step
       // wizardObj.stop();
     });
-
+    
     // Change event
     this.wizard.on('change', () => {
+      //console.log(this.wizard.getStep());
+      if (this.wizard.getStep() == 4) {
+        this.updateSampleInfo();
+      } else if (this.wizard.getStep() == 1) {
+        this.uploadSampleFile();
+      }
       setTimeout(() => {
         KTUtil.scrollTop();
       }, 500);
     });
   }
-
+  updateSampleInfo() {
+    let sampleData = {
+      "sampleId" : this.sampleId,
+      "name" : this.name,
+      "category" : this.category,
+      "tags" : this.tags,
+      "thumbnail" : this.thumbnail[0],
+      "link" : this.launchUrl[0],
+    }
+    let saveSub = this.commonService.fetchRow("sample/update", sampleData).subscribe(res => {
+      this.messageService.clear()
+      this.messageService.add({ severity: 'success', summary: 'Successfully', detail: 'Uploaded Sample', life: 3000 });
+    })
+    this.subscriptions.push(saveSub)
+  }
   uploadSampleFile(){
     // fileName
     let random = Math.floor((Math.random() * 9999) + 1);
@@ -109,7 +142,7 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
       console.log(this.files);
       let fileDetails = { 
         "name": random,
-        "link": random,
+        "link": 777,
         "tags": [],
         "category": random,
         "include_portfolio": [random],
@@ -119,6 +152,7 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
        };
       let fileSub = this.commonService.fetchRow("sample/create", fileDetails).subscribe(res => {
         console.log(res, 'file res');
+        this.sampleId = res._id;
         this.uploadSampleFilePutCall(res.upload_uri);
       });
       this.subscriptions.push(fileSub);
@@ -127,12 +161,35 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
 
 
   uploadSampleFilePutCall(upload_uri) {
-    let fileData = new FormData();
-    fileData.append("file", this.files[0]);
-    let fileSub = this.commonService.putRow(upload_uri, fileData).subscribe(res => {
+    let fileSub = this.commonService.putRow(upload_uri, this.files[0]).subscribe(res => {
         console.log(res, 'file res frin s3');
+        this.saveSampleFile();
     });
     this.subscriptions.push(fileSub);
+  }
+
+  saveSampleFile() {
+    let data = {
+      "sampleId" : this.sampleId
+    };
+    let saveSub = this.commonService.patchRow("sample/upload", data).subscribe(res => {
+      console.log(res, 'sample response');
+      this.messageService.clear()
+      this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'File uploaded successfully', life: 2000 });
+      this.getUploadedSampleDetails()
+      //this.wizard.goNext()
+    });
+    this.subscriptions.push(saveSub);
+  }
+
+  getUploadedSampleDetails() {
+    let getSub = this.commonService.getRow(`sample/${this.sampleId}`).subscribe(res => {
+      console.log(res, 'sample response');
+      this.uploadedFiles = res.uploadedFiles.filter(file => !file.includes(".jpg") && !file.includes(".png"));
+      this.uploadedImageFiles = res.uploadedFiles.filter(file => file.includes(".jpg") || file.includes(".png"));
+      this.cd.detectChanges();
+    });
+    this.subscriptions.push(getSub);
   }
 
   onSubmit() {
@@ -154,6 +211,7 @@ export class AddSampleComponent implements OnInit, AfterViewInit, OnDestroy {
     this.files.splice(0,1); // index =0 , remove_count = 1
     }
 
+  
   onRemove(event) {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
